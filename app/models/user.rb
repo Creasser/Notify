b5f4ee6f2bbb7b865b78ad297f352936e6d46e9e
Creasser <blakecreasser@aggienetwork.com>
2023-12-07 18:55:46 -0600

    create and run chatroom model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username, uniqueness: true, presence: true, length: {in: 2..20}
+    scope :all_exept, ->(user) {where.not(id: user)}
 end
